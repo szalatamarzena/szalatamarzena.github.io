15f5259947764f4eb8eb3939b412eda3341319f2
Marzena Szałata <szalatamarzena@gmail.com>
2024-06-03 09:34:28 +0200

    redesing portfolio

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-window.onload = function () {
-    let elements = document.querySelectorAll('.animation');
-    document.addEventListener('scroll', callback);
+// window.onload = function () {
+//     let elements = document.querySelectorAll('.animation');
+//     document.addEventListener('scroll', callback);
 
-    function callback() {
-        let windowHeight = window.innerHeight;
-        for (let element of elements) {
-            var positionFromTop = element.getBoundingClientRect().top;
-            if (positionFromTop - windowHeight <= 0) {
-                element.classList.add('animation-right');
-            }
-        }
-    }
-}
+//     function callback() {
+//         let windowHeight = window.innerHeight;
+//         for (let element of elements) {
+//             var positionFromTop = element.getBoundingClientRect().top;
+//             if (positionFromTop - windowHeight <= 0) {
+//                 element.classList.add('animation-right');
+//             }
+//         }
+//     }
+// }
