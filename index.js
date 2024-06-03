334f9177c74571e918e07f7792a55331f6cc75ff
Marzena Szałata <53177052+szalatamarzena@users.noreply.github.com>
2024-06-03 09:42:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// window.onload = function () {
-//     let elements = document.querySelectorAll('.animation');
-//     document.addEventListener('scroll', callback);
-
-//     function callback() {
-//         let windowHeight = window.innerHeight;
-//         for (let element of elements) {
-//             var positionFromTop = element.getBoundingClientRect().top;
-//             if (positionFromTop - windowHeight <= 0) {
-//                 element.classList.add('animation-right');
-//             }
-//         }
-//     }
-// }
+// Work in progress
